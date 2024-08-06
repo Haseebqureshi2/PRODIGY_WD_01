e3f02001dd7b8099822ec5f56e348bc412e86f7a
Haseebqureshi2 <haseebqureshi5152@gmail.com>
2024-08-06 12:03:54 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.style.backgroundColor = '#222';
+    } else {
+        navbar.style.backgroundColor = '#333';
+    }
+});
